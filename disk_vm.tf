3f7682ec3c52e9c949bc0a84f8af6802e8043eb6
gilyazov-ranel <ranel.yes@yandex.ru>
2024-12-09 19:24:05 +0000

    commit

--- disk_vm.tf
+++ disk_vm.tf
@@ -23,9 +23,10 @@ resource "yandex_compute_instance" "storage" {
   dynamic "secondary_disk" {
    for_each = "${yandex_compute_disk.disk.*.id}"
    content {
- 	disk_id = yandex_compute_disk.disk["${secondary_disk.key}"].id
+ 	    disk_id = secondary_disk.value
    }
   }
+
   network_interface {
 	subnet_id = yandex_vpc_subnet.develop.id
 	nat   	= true
